74346a7ccf6716a785ba00f14e8aa0901fef68f6
Adeola Ajala <ajalaadeola@gmail.com>
2025-06-22 08:20:07 +0200

    Create objects for indexing

--- Backend/search_engine/model.py
+++ Backend/search_engine/model.py
@@ -12,7 +12,37 @@ class WebPage(BaseModel):
     class config:
         schema_extra={
             "example":{
-                "links":"www.example.om",
+                "links":"www.example.com",
                 "index": "compute by hash algorith"
+            }
+        }
+
+class Pages(BaseModel):
+    """Object of web page"""
+    url: str # url of web page
+    wordCount: int # how many times a word appear on the web page
+    title: str # web page title
+
+    class config:
+        schema_extra={
+            "example":{
+                "url":"www.example.com",
+                "wordCount": 2,
+                "title": "Example"
+            }
+        }
+
+class Indexing(BaseModel):
+    """Object"""
+    token: str # word
+    webPages: Pages 
+
+    class config:
+        schema_extra={
+            "example":{
+                "token":"Python",
+                "webPage": {"url":"www.example.com",
+                            "wordCount": 2,
+                            "title": "Example"}
             }
         }
